d0aaa3c77883b982d67fa53c6cfc8ff1028ed1c7
surmistry <mistrs8@gmail.com>
2020-05-12 18:25:51 -0400

    Update bin require paths.

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const { hexagon } = require('./lib/templates');
-const { writeFile } = require('./lib/utils');
+const { hexagon } = require('../lib/templates');
+const { writeFile } = require('../lib/utils');
 const commandLineArgs = require('command-line-args')
 
 const optionDefinitions = [
