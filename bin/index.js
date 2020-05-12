29c127540b7c8a278365fcc1dd7f87b7971ffcb6
surmistry <mistrs8@gmail.com>
2020-05-12 18:21:52 -0400

    Implement as npm package and finalize hexagon feature

---
+++ bin/index.js
@@ -0,0 +1,19 @@
+const { hexagon } = require('./lib/templates');
+const { writeFile } = require('./lib/utils');
+const commandLineArgs = require('command-line-args')
+
+const optionDefinitions = [
+  { name: 'start', type: String },
+  { name: 'end', type: String, },
+  { name: 'noise', alias: 'n', type: Number },
+  { name: 'width', alias: 'w', type: Number },
+  { name: 'height', alias: 'h', type: Number },
+  { name: 'size', alias: 's', type: Number },
+  { name: 'output', alias: 'o', type: string }
+]
+
+const options = commandLineArgs(optionDefinitions)
+const { start, end, noise, width, height } = options;
+const hexString = hexagon({ colors: { start, end }, shapeSize: 15, width, height, noise });
+writeFile(options.output || './output.svg', hexString);
+
--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const { hexagon } = require('./lib/templates');
-const { writeFile } = require('./lib/utils');
-
-const hexString = hexagon({ shapeSize: 15, viewBox: [200, 115], noise: 50 });
-writeFile('./output.svg', hexString);
+module.exports = require('./lib/templates')
